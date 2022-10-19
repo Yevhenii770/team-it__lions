d0458f796c1feb21b401c241cd84aa7f80fe63e4
VychopenNazar <110553042+VychopenNazar@users.noreply.github.com>
2022-10-19 21:08:53 +0300

    1

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,18 +1,11 @@
-// $('.slick').slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 500,
-//   fade: true,
-//   cssEase: 'linear',
+$('.slick').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
 
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-// });
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
 
-// $('.one-time').slick({
-//   dots: true,
-//   infinite: true,
-//   speed: 300,
-//   slidesToShow: 1,
-//   adaptiveHeight: true
-// });
