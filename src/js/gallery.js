6f9f352180703248cd04d7dbdfb8828082db3430
SKrumilenko <skrumilenko@gmail.com>
2022-10-20 20:43:10 +0300

    change sliders

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,9 +1,10 @@
-// $('.slick').slick({
-//   infinite: true,
-//   speed: 500,
-//   fade: true,
-//   cssEase: 'linear',
-
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-// });
+$('.slick').slick({
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+});
