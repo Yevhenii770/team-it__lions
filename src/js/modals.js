e7b0f275ff3a1f2304d238fcc2da85959b679c65
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-05-09 23:08:15 +0200

    deleted empty js file

--- src/js/modals.js
+++ src/js/modals.js
@@ -26,34 +26,23 @@
 })(window.Element.prototype);
 
 document.addEventListener('DOMContentLoaded', function () {
-  /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
   var modalButtons = document.querySelectorAll('.js-open-modal'),
     overlay = document.querySelector('.js-overlay-modal'),
     closeButtons = document.querySelectorAll('.js-modal-close');
 
-  /* Перебираем массив кнопок */
   modalButtons.forEach(function (item) {
-    /* Назначаем каждой кнопке обработчик клика */
     item.addEventListener('click', function (e) {
-      /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
       e.preventDefault();
 
-      /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
       var modalId = this.getAttribute('data-modal'),
         modalElem = document.querySelector(
           '.modal[data-modal="' + modalId + '"]'
         );
 
-      /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
       modalElem.classList.add('active');
       overlay.classList.add('active');
-    }); // end click
-  }); // end foreach
+    });
+  });
 
   closeButtons.forEach(function (item) {
     item.addEventListener('click', function (e) {
@@ -62,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
     });
-  }); // end foreach
+  });
 
   document.body.addEventListener(
     'keyup',
@@ -81,4 +70,4 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.modal.active').classList.remove('active');
     this.classList.remove('active');
   });
-}); // end ready
+});
